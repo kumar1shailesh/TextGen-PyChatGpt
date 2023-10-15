0ecad88ba35be8a4355423799c067daaf9fb8dc5
Shailesh Kumar Yadav <111674342+kumar1shailesh@users.noreply.github.com>
2023-10-16 01:49:45 +0530

    Create TextGen-PyChatGpt-basiccode2.py

---
+++ TextGen-PyChatGpt-basiccode2.py
@@ -0,0 +1,12 @@
+import openai
+
+openai.api_key = 'sk-UbuY8SpGpqXXXXX'
+
+// instead of text-davinci-003 you can use latest gpt-3.5-turbo Learn More https://www.youtube.com/@AIMLDLNLP-TECH/
+response = openai.Completion.create(
+  engine="text-davinci-003",
+  prompt="What dinosaurs lived in the cretaceous period?",
+  max_tokens=60
+)
+
+print(response.choices[0].text.strip())
